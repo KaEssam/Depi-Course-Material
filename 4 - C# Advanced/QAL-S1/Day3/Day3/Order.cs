2e5c0aedd5c690521ef5233a4a12e36820062f3a
Karim Essam <kaessam17@gmail.com>
2025-08-29 23:41:00 +0300

    Add LINQ DATA project with library management system implementation
    - Created solution file for LINQ DATA project.
    - Implemented ConsoleTableExtensions for formatted console output of collections.
    - Added LibraryData class with entities for Book, Author, Member, and Loan, including sample data.
    - Developed Program.cs to demonstrate LINQ queries for available books and book details with authors.
    - Configured project file for .NET 9.0 with necessary properties.

---
+++ 4 - C# Advanced/QAL-S1/Day3/Day3/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    public delegate decimal DicountRule(Order order);
+    public class Order
+    {
+        public decimal SubTotal { get; set; }
+        public decimal Shipping { get; set; }
+        public decimal Discount { get; set; }
+        public decimal Total => SubTotal + Shipping - Discount;
+    }
+}
